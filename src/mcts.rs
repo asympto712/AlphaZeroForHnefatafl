6931cb52250128152a387c00e901d0c72f4a3513
Keigo Kurokawa <keigo20736@gmail.com>
2025-01-28 10:09:12 +0100

    finished mcts.rs(not tested),modified some of the directories

--- src/mcts.rs
+++ src/mcts.rs
@@ -1,237 +1,182 @@
+#![allow(non_snake_case)]
+
 use std::collections::HashMap;
-
 use rand::prelude::*;
+use tch::{CModule, Tensor, Kind, Device};
 
+type Action = u32;
+type State = Vec<u32>;  // Here we assume the state is already converted into matrix representation & flattened 
 
-// MCTS implementation. The code below is far from complete, some missing functions, assume the existence of game code, etc..
-// I might drastically change this, especially the Node struct (whose change would propagate to the whole thing)
-#[derive(Debug, Clone)]
-struct Node<Action, State> {
-    state: State, // Replace with your actual game state type
+const C_PUCT: f32 = 0.3;
+
+#[derive(Debug, Clone, Copy)]
+struct Node {
+    state: State, 
     parent: Option<(Node<Action, State>, Action)>,
-    children: HashMap<Action, Box<Node<Action, State>>>, // Replace with the actual action type // We want to record not only the child nodes but also how to get to that node
-    visits: f64,
-    valid_actions: Vec<Action>, // I have a feeling that we don't need this
-    action_probs: HashMap<Action, f64>,
-    action_counts: HashMap<Action, f64>, // maybe we don't need this cause it's a tree?
-    action_Qs: HashMap<Action, f64>,
-
-
-
-    // // (s,a) (=> s') if we regard (s,a) as the next state s', then no need to assign action_probs/Qs?
-    // // Idk if it's ultimately better.
-    // update: I think whichever is fine, as long as I stick to my choice. I will go with using hashmaps for probs, Qs, counts...
-    // IMPORTANT: using Action as the key type is most likely unwise, for nodes other than the root we don't need to have complete knowledge of the actions,
-    // so I think I should just use more suitable (like u32) type for key.
-    // prob: f64,
-    // Q_value: f64,
+    children: HashMap<Action, Box<Node<Action, State>>>, 
+    visits: f32,
+    valid_actions: Vec<Action>, 
+    action_probs: HashMap<Action, f32>,
+    action_counts: HashMap<Action, f32>, 
+    action_Qs: HashMap<Action, f32>,
 }
 
-impl<Action: Eq + std::hash::Hash, State> Node<Action, State> {
+impl Node{
     fn new(state: State,
-           parent: Option<Node<Action, State>>,
+           parent: Option<(Node<Action, State>, Action)>,
            valid_actions: Vec<Action>,
-           visits: f64) -> Self {
+           visits: f32) -> Self {
         Node {
             state,
             parent,
             children: HashMap::new(),
+            visits,
             valid_actions,
             action_probs: HashMap::new(),
             action_counts: HashMap::new(),
             action_Qs: HashMap::new(),
-
-            // prob: 
-            // visits: 0.0,
         }
     }
 
-    fn uct_value(&self, action: Action) -> f64 {
-        if self.action_counts(action) == 0.0 {
-            return f64::INFINITY;
-        }
+    fn uct_value(&self, action: &Action) -> f32 {
+        // if self.action_counts(action) == 0.0 {
+        //     return f64::INFINITY;
+        // }
         // To do; add some dirichlet noise
-        self.action_Qs(action) + c_puct * self.action_probs(action) * self.visits.sqrt() / (1 + self.action_counts(action));
+        return self.action_Qs(action) + C_PUCT * self.action_probs(action) * self.visits.sqrt() / (1 + self.action_counts(action));
     }
 
-    // fn uct_value(&self) -> f64 {
-    //     if self.visits == 0.0 {
-    //         return f64::INFINITY;
-    //     }
-    //     let parent_visit = match self.parent {
-    //         Some(parent) => parent.visits ,
-    //         None(_) => 0.0 // Just a placeholder value. This case need not be considered. 
-    //     }
-    //     // .....
-    // }
 }
 
-// struct Tree {
-//     nodes: HashSet<State>, // replace with the actual type for the game states
-//     root: Node,
-// }
-
-// impl Tree {
-//     fn new(root_state: String) -> Self {
-//         let mut nodes = HashMap::new();
-//         let root = Node::new(root_state, None);
-//         nodes.insert(0, root);
-//         Tree { nodes, root: 0 }
-//     }
-
-//     // Cannot trust. 
-//     fn select(&self) -> usize {
-//         let mut node_id = self.root;
-//         while !self.nodes[&node_id].children.is_empty() {
-//             node_id = *self.nodes[&node_id]
-//                 .children
-//                 .iter()
-//                 .max_by(|&&a, &&b| {
-//                     self.nodes[&a]
-//                         .uct_value(total_visits)
-//                         .partial_cmp(&self.nodes[&b].uct_value(total_visits))
-//                         .unwrap()
-//                 })
-//                 .unwrap();
-//         }
-//         node_id
-//     }
-// }
-
-// performs MCTS search until a new node is found. String should be replaced with the actual type for the actions
-fn search(&self) -> (Node, String) {
-    let mut node = &mut self.root;
-    while true {
-        let next_action = node.valid_actions
-            .iter()
-            .max_by(|a, b| {
-                node.uct_value(a).partial_cmp(node.uct_value(b)).unwrap()
-            })
-            .unwrap();
-        
-        let next_state = node.do_move(next_action);
-
-        if game.Endstate(next_state) {
-            let reward = game.reward(next_state);
-            expand(node, next_state);
-            backpropagate(node, reward);
-            break;
-        }
-        // If the new_state is unreached
-        if !node.children.contains_key(next_action) {
-            let (prob, reward) = nn.pred(next_state);
-            expand(node, action, next_state);
-            backpropagate(node, reward);
-            break;
-        }
-        node = node.children.get(&next_action).unwrap();
-    }
-}
-
-fn search(game: Game, node: Node) -> f64 {
+// Current one.
+fn search(game: &mut Game, node: &mut Node, nnmodel: &CModule) -> f32 {
 
     if game.is_terminal(node.state) {
-        let reward = game.reward(Node.state);
-        return -reward;
+        let reward = game.player * game.reward(Node.state); // Assumes game.reward returns the reward from the ATTACKER's perspective
+        return -1 * reward;
     }
 
     let action = node.valid_actions
             .iter()
             .max_by(|a, b| {
-                node.uct_value(a).partial_cmp(node.uct_value(b)).unwrap()
+                node.uct_value(a).partial_cmp(&node.uct_value(b)).unwrap()
             })
             .unwrap();
-    let next_state = game.get_next_state(action);
+    
+    game.do_move(action);
         
-    if !node.children.contains_key(action) {
-        let (prob, reward) = nn.predict(next_state); //TODO;  use different heads depending on who's playing
-        expand(node, action, next_state);
-        return -reward
+    if !node.children.contains_key(action) {         
+        let reward = expand(&mut node, &action, &game, &nnmodel);
+        return -1 * reward
     }
 
-    let next_node = node.children(action);
-    let reward = search(game, next_node);
+    let next_node = **node.children.get(action).unwrap();
+    let reward = search(game, &mut next_node, &nnmodel);
 
     node.actions_Qs(action) = (node.actions_counts(action) * node.actions_Qs(actions) + reward) / (node.actions_counts(action) + 1.0);
     node.actions_counts(action) += 1.0;
     node.visits += 1.0;
-    return -reward
+    return -1 * reward
     
 }
 
-// fn expand(&mut self, node_id: usize, new_state: String) -> usize {
-//     let new_node_id = self.nodes.len();
-//     let new_node = Node::new(new_state, Some(node_id));
-//     self.nodes.get_mut(&node_id).unwrap().children.push(new_node_id);
-//     self.nodes.insert(new_node_id, new_node);
-//     new_node_id
-// }
+fn expand(parent: &mut Node<Action, State>, action: &Action, game: &Game, nnmodel: &CModule) {
+    let (valid_actions, action_probs, reward) = model_predict(game, nnmodel);
+    let num_valid_actions = valid_actions.sum();
+    let mut action_counts = HashMap::with_capacity(num_valid_actions);
+    let mut action_Qs = HashMap::with_capacity(num_valid_actions);
+    for action in valid_actions {
+        action_counts.insert(*action, 0.0);
+        action_Qs.insert(*action, 0.0);
+    }
 
-fn expand(parent: &mut Node<Action, State>, action: &Action, next_state: &State) {
-    let (prob, reward) = nnmodel.predict(next_state);
-    let valid_actions = game.get_valid_actions(next_state); // We don't need this?
-    let valid_actions_for_masking = game.get_valid_actions_for_masking(next_state); //Ideally this should be a vector of {0,1}.
-    let mut actions_probs = prob * valid_actions_for_masking; // extract only the valid actions. Todo: make this operation element-wise
-    let sum_prob = actions_probs.sum();
-    if sum_prob > 0 {
-        actions_prob /= sum_prob;                           // renormalize
+    let mut new_node: Node<Action, State> = Node{
+        state: game.state,
+        parent: (parent, action),
+        children: HashMap::new(),
+        visits: 1.0,
+        valid_actions,
+        action_probs,
+        action_counts,
+        action_Qs,
+    };
+    parent.children.insert(action, Box::new(new_node));
+
+    return reward
+}
+
+fn model_predict(game: &Game, nnmodel: &CModule) -> (Vec<f32>, Vec<f32>, f32) {
+
+    // Preparing input Tensors
+    let board = Tensor::of_data(game.state, &[7,7], (Kind::Float32, Device::Cpu));
+    let cond: [bool; 1] = if game.player == 1 {[true]} else {[false]};
+    let cond: Tensor = Tensor::from_slice(&cond);
+
+    let (prob, reward): (Tensor, Tensor) = nnmodel.forward_is(&[&[board], &[cond]]).try_into().unwrap();
+
+    // Converting outputs into vectors
+    let prob = prob.flatten(0, i64::try_from(prob.size().len()).unwrap() - 1);
+    let prob = Vec::<f32>::try_from(prob).expect("Something went wrong when converting tensor into vector");
+    let reward = reward.flatten(0, i64::try_from(reward.size().len()).unwrap() - 1);
+    let reward = f32::try_from(reward).expect("Could not convert value tensor to f32");
+
+    let valid_actions = game.get_valid_actions(game.state);
+    let valid_actions_for_masking: Vec<f32> = game.get_valid_actions_for_masking(game.state).try_into().expect("could not convert mask into Vec<f32>"); 
+    // This should output a correct valid moves depending on the variable game (-> whose turn it is)  
+
+    let mut action_probs: Vec<f32> = prob.iter()
+        .zip(valid_actions_for_masking.iter())
+        .map(|(p, v)| p * v)
+        .collect();
+
+    let sum_probs = actions_probs.sum();
+    if sum_probs > 0 {
+        actions_probs /= sum_probs;                           // renormalize
     } else {                                                // Contingency for when all the actions with non-zero probabilities are masked
         let num_valid_actions = valid_actions_for_masking.sum();
         actions_probs = valid_actions_for_masking / num_valid_actions;
     }
+    return (valid_actions, action_probs, reward)
+}
 
-    let mut new_node: Node<Action, State> = Node{
-        state: next_state,
-        parent: parent,
-        children: HashMap::new(),
-        visits: 1.0,
-        valid_actions: valid_actions,
-        actions_probs: actions_prob,
-        actions_counts: HashMap::new(),
-        actions_Qs: HashMap::new(),
+fn get_improved_policy(root: Node) -> Vec<f32> {
+    let pi_size = powd(game.size, 4);
+    let mut pi = vec![0.0; pi_size];
+    let count_sum: f32 = root.children.values().map(|child| child.visits).sum();
+    for (action, child) in &root.children {
+        let index = action_to_index(action); 
+        pi[index] = child.visits / count_sum;
+    }
+    pi
+}
+
+// This does a single mcts starting from whomever the current turn is assigned to  
+fn mcts(nnmodel: CModule, game: Game, iterations: usize) -> Vec<f32> {
+    let (valid_actions, action_probs, reward) = model_predict(game, &nnmodel);
+    let root_state = game.state.copy();
+    let num_valid_actions = valid_actions.sum();
+    let mut action_counts = HashMap::with_capacity(num_valid_actions);
+    let mut actions_Qs = HashMap::with_capacity(num_valid_actions);
+    for action in valid_actions {
+        action_counts.insert(action, 0.0);
+        action_Qs.insert(action, 0.0);
+    }
+
+    let root = Node {
+        state: root_state,
+        parent: None,
+        children: HashMap::with_capacity(num_valid_actions),
+        visits: 1,
+        valid_actions,
+        action_probs,
+        action_Qs,
+        action_counts,
     };
-    parent.children.insert(action, Box::new(new_node));
-}
 
-// fn backpropagate(&mut self, node_id: usize, result: f64) {
-//     let mut current = Some(node_id);
-//     while let Some(id) = current {
-//         let node = self.nodes.get_mut(&id).unwrap();
-//         node.visits += 1.0;
-//         node.wins += result;
-//         current = node.parent;
-//     }
-// }
-
-// We might not need this (incorporated into fn search)
-fn backup(leaf_node: Node<Action, State>, reward: f64) {
-    let mut node: Node<Action, State> = leaf_node;
-    while let Some((parent, action)) = node.parent {
-        node = parent;
-        node.actions_Qs = (node.actions_counts(action) * node.actions_Qs(actions) + reward) / (node.actions_counts(action) + 1);
-        node.actions_counts(action) += 1;
-    }
-}
-
-fn best_move(&self) -> usize {
-    *self.nodes[&self.root]
-        .children
-        .iter()
-        .max_by(|&&a, &&b| {
-            self.nodes[&a]
-                .visits
-                .partial_cmp(&self.nodes[&b].visits)
-                .unwrap()
-        })
-        .unwrap()
-}
-
-fn mcts(&mut self, iterations: usize) {
     for _ in 0..iterations {
-        let selected_node = self.select();
-        let new_state = "new_state".to_string(); // Replace with your actual state generation logic
-        let expanded_node = self.expand(selected_node, new_state);
-        let result = self.simulate(expanded_node);
-        self.backpropagate(expanded_node, result);
+        let game_copy = game.copy();
+        let _ = search(game_copy, &mut root, &nnmodel);
     }
+
+    get_improved_policy(root)
 }
